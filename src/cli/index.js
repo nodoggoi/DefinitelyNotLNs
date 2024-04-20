bd8c7725c87499eeffd28245839bbd09b1ecdeeb
Mampfinator <46071499+Mampfinator@users.noreply.github.com>
2024-04-20 20:17:16 +0200

    add and run prettier

--- src/cli/index.js
+++ src/cli/index.js
@@ -1,150 +1,159 @@
 #!/usr/bin/env node
-const readline = require("readline");
+const readline = require('readline')
 
-const yargs = require("yargs/yargs");
-const {hideBin} = require("yargs/helpers");
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
 
-const { NeoSekaiScraper } = require("../neosekai");
-const { joinChapterContent } = require("../util");
+const { NeoSekaiScraper } = require('../neosekai')
+const { joinChapterContent } = require('../util')
 
 yargs(hideBin(process.argv))
-    .command("list <service> [novel]", "List things!", {
-        service: {
-            type: "string",
-            describe: "The service to list.",
-            demandOption: true,
+    .command(
+        'list <service> [novel]',
+        'List things!',
+        {
+            service: {
+                type: 'string',
+                describe: 'The service to list.',
+                demandOption: true,
+            },
+            novel: {
+                type: 'string',
+                describe: 'The novel to list chapters of. If not specified, lists all novels instead.',
+            },
         },
-        novel: {
-            type: "string",
-            describe: "The novel to list chapters of. If not specified, lists all novels instead."
-        }
-    }, async (argv) => {
-        if (argv.service !== "neosekai") return console.log("Invalid service.");
+        async (argv) => {
+            if (argv.service !== 'neosekai') return console.log('Invalid service.')
 
-        const scraper = new NeoSekaiScraper();
-        
-        const novelPath = argv.novel;
+            const scraper = new NeoSekaiScraper()
 
-        if (!novelPath) {
-            const novels = await scraper.getNovelList();
-            printNovelList(novels);
-            return;
-        }
+            const novelPath = argv.novel
 
-        const chapters = await scraper.getChapterList(novelPath);
-
-        if (!chapters) {
-            console.log("Novel not found!");
-            return;
-        }
-
-        printChapterList(chapters);
-
-
-    })
-    .command("read <service> [novel] [chapter]", "Read a novel", {
-        service: {
-            type: "string",
-            describe: "The service to read from. Currently only supports 'neosekai'.",
-            demandOption: true,
-        },
-        novel: {
-            type: "string",
-            demandOption: false,
-            describe: "The novel to read. If not specified, you will be prompted to select one. This should be the path of the novel as seen in the adressbar of the website, not the title."
-        },
-        chapter: {
-            type: "string",
-            demandOption: false
-        },
-        listReverse: {
-            type: "boolean",
-            demandOption: false,
-            default: false,
-            describe: "Reverse the order of the chapters. Lists the older chapters first."
-        }
-    }, async (argv) => {
-        if (argv.service !== "neosekai") return console.log("Invalid service.");
-
-        const scraper = new NeoSekaiScraper();
-
-        let {novel, chapter} = argv;
-
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
-
-        if (!novel) {
-            const novels = await scraper.getNovelList();
-
-            printNovelList(novels);
-
-            const answer = await question("Which novel do you want to read? ", rl)
-            let novelIndex = parseInt(answer) - 1;
-
-            if (isNaN(novelIndex)) {
-                if (!answer) return console.log("Invalid input");
-
-                novelIndex = novels.findIndex(novel => novel.path === answer);
+            if (!novelPath) {
+                const novels = await scraper.getNovelList()
+                printNovelList(novels)
+                return
             }
 
-            if (novelIndex <= 0 || novelIndex >= novels.length) return console.log("Invalid index or path.");
+            const chapters = await scraper.getChapterList(novelPath)
 
-            novel = novels[novelIndex].path;
-        }
-
-        if (!chapter) {
-            const chapters = await scraper.getChapterList(novel);
-
-            if (argv.listReverse) {
-                chapters.reverse();
+            if (!chapters) {
+                console.log('Novel not found!')
+                return
             }
 
-            printChapterList(chapters);
+            printChapterList(chapters)
+        },
+    )
+    .command(
+        'read <service> [novel] [chapter]',
+        'Read a novel',
+        {
+            service: {
+                type: 'string',
+                describe: "The service to read from. Currently only supports 'neosekai'.",
+                demandOption: true,
+            },
+            novel: {
+                type: 'string',
+                demandOption: false,
+                describe:
+                    'The novel to read. If not specified, you will be prompted to select one. This should be the path of the novel as seen in the adressbar of the website, not the title.',
+            },
+            chapter: {
+                type: 'string',
+                demandOption: false,
+            },
+            listReverse: {
+                type: 'boolean',
+                demandOption: false,
+                default: false,
+                describe: 'Reverse the order of the chapters. Lists the older chapters first.',
+            },
+        },
+        async (argv) => {
+            if (argv.service !== 'neosekai') return console.log('Invalid service.')
 
-            const index = parseInt(await question("Which chapter do you want to read? ", rl));
+            const scraper = new NeoSekaiScraper()
 
-            const id = chapters[index - 1]?.id;
+            let { novel, chapter } = argv
 
-            if (!id) {
-                console.log("Invalid index");
-                return;
+            const rl = readline.createInterface({
+                input: process.stdin,
+                output: process.stdout,
+            })
+
+            if (!novel) {
+                const novels = await scraper.getNovelList()
+
+                printNovelList(novels)
+
+                const answer = await question('Which novel do you want to read? ', rl)
+                let novelIndex = parseInt(answer) - 1
+
+                if (isNaN(novelIndex)) {
+                    if (!answer) return console.log('Invalid input')
+
+                    novelIndex = novels.findIndex((novel) => novel.path === answer)
+                }
+
+                if (novelIndex <= 0 || novelIndex >= novels.length) return console.log('Invalid index or path.')
+
+                novel = novels[novelIndex].path
             }
 
-            chapter = id;
-        }
+            if (!chapter) {
+                const chapters = await scraper.getChapterList(novel)
 
-        const content = await scraper.getChapterContent(novel, chapter);
+                if (argv.listReverse) {
+                    chapters.reverse()
+                }
 
-        if (!content) {
-            console.log("Chapter not found");
-            return;
-        }
+                printChapterList(chapters)
 
-        const text = joinChapterContent(content);
-        console.log(text);
-    })
-    .parse();
+                const index = parseInt(await question('Which chapter do you want to read? ', rl))
+
+                const id = chapters[index - 1]?.id
+
+                if (!id) {
+                    console.log('Invalid index')
+                    return
+                }
+
+                chapter = id
+            }
+
+            const content = await scraper.getChapterContent(novel, chapter)
+
+            if (!content) {
+                console.log('Chapter not found')
+                return
+            }
+
+            const text = joinChapterContent(content)
+            console.log(text)
+        },
+    )
+    .parse()
 
 function question(question, interface) {
     return new Promise((resolve) => {
-        interface.question(question, resolve);
-    });
+        interface.question(question, resolve)
+    })
 }
 
 function printNovelList(novels) {
-    let i = 1;
-    for (const {title, path, thumb} of novels) {
-        console.log(`${i} - ${title} (${path})`);
-        i += 1;
+    let i = 1
+    for (const { title, path, thumb } of novels) {
+        console.log(`${i} - ${title} (${path})`)
+        i += 1
     }
 }
 
 function printChapterList(chapters) {
-    let i = 1;
-    for (const {title, id} of chapters) {
-        console.log(`${i} - ${title} (${id})`);
-        i += 1;
+    let i = 1
+    for (const { title, id } of chapters) {
+        console.log(`${i} - ${title} (${id})`)
+        i += 1
     }
 }
--- src/neosekai.js
+++ src/neosekai.js
@@ -1,7 +1,7 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
+const axios = require('axios')
+const cheerio = require('cheerio')
 
-const NEOSEKAI_BASE_URL = "https://www.neosekaitranslations.com";
+const NEOSEKAI_BASE_URL = 'https://www.neosekaitranslations.com'
 
 /**
  * Scraper for [NeoSekai translations](https://www.neosekaitranslations.com/).
@@ -10,82 +10,98 @@ class NeoSekaiScraper {
     constructor() {
         this.instance = axios.create({
             baseURL: NEOSEKAI_BASE_URL,
-            withCredentials: true
-        });
+            withCredentials: true,
+        })
     }
 
     /**
      * @returns {Promise<{title: string, path: string, thumb: string}[]>}
      */
     async getNovelList() {
-        const $ = await this.instance({url: "novel"}).then(res => cheerio.load(res.data));
+        const $ = await this.instance({ url: 'novel' }).then((res) => cheerio.load(res.data))
 
-        const novels = [];
+        const novels = []
 
-        $(".page-item-detail.text").each((_, el) => {
-            const $el = $(el);
+        $('.page-item-detail.text').each((_, el) => {
+            const $el = $(el)
 
-            const thumb = $el.find("img").attr("src");
-            const title = $el.find("h3.h5").text().replaceAll(/[\n\t]/g, "").trim().replaceAll(/^(HOT|NEW) */g, "");
-            const url = $el.find("h3 > a").attr("href");
+            const thumb = $el.find('img').attr('src')
+            const title = $el
+                .find('h3.h5')
+                .text()
+                .replaceAll(/[\n\t]/g, '')
+                .trim()
+                .replaceAll(/^(HOT|NEW) */g, '')
+            const url = $el.find('h3 > a').attr('href')
 
-            const path = url.replace(NEOSEKAI_BASE_URL + "/novel", "").replaceAll("/", "");
+            const path = url.replace(NEOSEKAI_BASE_URL + '/novel', '').replaceAll('/', '')
 
             novels.push({
                 title,
                 path,
                 thumb,
-            });
-        });
+            })
+        })
 
-        return novels;
+        return novels
     }
 
     /**
      * Returns the list of chapters for a given novel.
-     * @param {string} novelPath 
+     * @param {string} novelPath
      * @returns {Promise<{title: string, url: string, id: string}>}
      */
     async getChapterList(novelPath) {
-        const $ = await this.instance({url: `novel/${novelPath}`}).then(res => cheerio.load(res.data));
+        const $ = await this.instance({ url: `novel/${novelPath}` }).then((res) => cheerio.load(res.data))
 
-        const $body = $("body");
+        const $body = $('body')
 
-        const mangaId = $body.attr("class").split(" ").find(c => c.startsWith("postid-")).replace("postid-", "");
+        const mangaId = $body
+            .attr('class')
+            .split(' ')
+            .find((c) => c.startsWith('postid-'))
+            .replace('postid-', '')
 
-        const $chapterList = await this.instance({url: "wp-admin/admin-ajax.php", data: {action: "manga_get_chapters", manga: mangaId}, method: "POST", headers: {
-            "Accept": "*/*",
-            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-            Cookie: "wpmanga-reading-history=W3siaWQiOjE1NTQsImMiOiIyNjE2IiwicCI6MSwiaSI6IiIsInQiOjE3MTM2MDgxNDd9XQ%3D%3D",
-            DNT: 1,
-            Origin: NEOSEKAI_BASE_URL,
-            Referrer: NEOSEKAI_BASE_URL + `/novel/${novelPath}`,
-            "X-Requested-With": "XMLHttpRequest"
-        }}).then(res => cheerio.load(res.data));
+        const $chapterList = await this.instance({
+            url: 'wp-admin/admin-ajax.php',
+            data: { action: 'manga_get_chapters', manga: mangaId },
+            method: 'POST',
+            headers: {
+                Accept: '*/*',
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                Cookie: 'wpmanga-reading-history=W3siaWQiOjE1NTQsImMiOiIyNjE2IiwicCI6MSwiaSI6IiIsInQiOjE3MTM2MDgxNDd9XQ%3D%3D',
+                DNT: 1,
+                Origin: NEOSEKAI_BASE_URL,
+                Referrer: NEOSEKAI_BASE_URL + `/novel/${novelPath}`,
+                'X-Requested-With': 'XMLHttpRequest',
+            },
+        }).then((res) => cheerio.load(res.data))
 
-        const chapters = [];
+        const chapters = []
 
-        $chapterList("li.wp-manga-chapter").each((i, el) => {
-            const $el = $(el);
-            
-            const $a = $el.find("a");
-            const fullTitle = $a.text();
+        $chapterList('li.wp-manga-chapter').each((i, el) => {
+            const $el = $(el)
 
-            const title = fullTitle.replace(/Chapter [0-9]+ +\- +/, "").trim();
+            const $a = $el.find('a')
+            const fullTitle = $a.text()
 
-            const url = $a.attr("href");
+            const title = fullTitle.replace(/Chapter [0-9]+ +\- +/, '').trim()
 
-            const id = url.split("/").filter(c => !!c).pop();
-            
+            const url = $a.attr('href')
+
+            const id = url
+                .split('/')
+                .filter((c) => !!c)
+                .pop()
+
             chapters.push({
                 title,
                 url,
                 id,
-            });
-            
-        });
+            })
+        })
 
-        return chapters;
+        return chapters
     }
 
     /**
@@ -95,31 +111,33 @@ class NeoSekaiScraper {
      */
     async getChapterContent(novelPath, chapterPath) {
         try {
-            const $ = await this.instance({url: `novel/${novelPath}/${chapterPath}`}).then(res => cheerio.load(res.data));
-            const content = [];
-    
+            const $ = await this.instance({ url: `novel/${novelPath}/${chapterPath}` }).then((res) =>
+                cheerio.load(res.data),
+            )
+            const content = []
+
             // p > span because the last paragraph contains a script to add a kofi button to the bottom, and we don't need that.
-            $(".entry-content p > span").each((i, el) => {
-                const paragraph = $(el).text().trim();
-    
+            $('.entry-content p > span').each((i, el) => {
+                const paragraph = $(el).text().trim()
+
                 if (paragraph.length > 0) {
-                    content.push(paragraph);
+                    content.push(paragraph)
                 }
-            });
-    
+            })
+
             if (content.length == 0) {
-                return null;
+                return null
             }
 
-            return content;
+            return content
         } catch (error) {
             if (error instanceof axios.AxiosError && error.response.status == 404) {
-                return null;
+                return null
             }
 
-            throw error;
+            throw error
         }
     }
 }
 
-module.exports = { NeoSekaiScraper };
+module.exports = { NeoSekaiScraper }
